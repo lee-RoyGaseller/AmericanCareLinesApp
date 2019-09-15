94fbd0e4381e6e56ff7dfdc1244d8b56c5c564d5
Lee-Roy Gaseller <leeroygaseller@gmail.com>
2019-09-15 09:57:59 -0500

    page connectivity done (with pages we have right now)

--- app/src/main/java/com/example/americancarelinesapp/AppInfo2.java
+++ app/src/main/java/com/example/americancarelinesapp/AppInfo2.java
@@ -2,7 +2,10 @@ package com.example.americancarelinesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class AppInfo2 extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class AppInfo2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_info2);
+        RelativeLayout rlayout = (RelativeLayout) findViewById(R.id.openingTwo);
+        rlayout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                moveOn();
+
+            }
+
+        });
+    }
+    public void moveOn() {
+        Intent intent = new Intent(this, AppInfo3.class);
+        startActivity(intent);
     }
 }
